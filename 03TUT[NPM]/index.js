ff63c1664a5dd59facb1440f6400367526b187ef
Archie Holmes <archie.holmes@gmail.com>
2022-08-04 13:16:24 +0100

    03TUT
    TUT3 on npm, update .gitignore for local sub-app

--- 03TUT[NPM]/index.js
+++ 03TUT[NPM]/index.js
@@ -5,3 +5,21 @@
 
 console.log("03TUT - testing...");
 console.log("Checking nodemon!");
+
+const { format } = require("date-fns");
+
+//9
+//ref: https://www.npmjs.com/package/uuid
+const { v4: uuid } = require("uuid");
+// means import v4 as uuid from uuid
+// use as uuid()
+
+// note format string: "yyyyMMdd\tHH:mm:ss" is tab delimitted :)
+
+// console.log("Testing (7) dev nodemon");
+
+//9 - we ant a log...
+console.log(format(new Date(), "yyyyMMdd\tHH:mm:ss"));
+console.log(uuid());
+
+console.log();
