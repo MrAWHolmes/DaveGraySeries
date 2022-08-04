039ebad24fd48168700ff128395cda4900865057
Archie Holmes <archie.holmes@gmail.com>
2022-08-04 10:18:30 +0100

    03TUT0
    check node_modules dont add

---
+++ 03TUT[NPM]/index.js
@@ -0,0 +1,7 @@
+//ref1:https://youtu.be/oGO_-DWTmKA
+
+// This is a simple file used in the demonstration
+// of NPM basics by Dave Gray
+
+console.log("03TUT - testing...");
+console.log("Checking nodemon!");
